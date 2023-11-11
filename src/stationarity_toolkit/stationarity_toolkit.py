fix variance stationarityplots to show whenever var stationarity is dealth with.

src/stationarity_toolkit/stationarity_toolkit.py
@@ -125,11 +125,13 @@ class StationarityToolkit:
             constant = (
                 1  # Choose a positive constant (can be adjusted based on the data)
             )
-            boxcox_transformed_data, lam = boxcox(ts.values.flatten() + constant)
-            transformations["Box-Cox Transformed"] = (
-                boxcox_transformed_data,
-                self.inv_boxcox,
-            )
+            bc_ts = ts.values.flatten() + constant
+            if np.all(bc_ts > 0):
+                boxcox_transformed_data, lam = boxcox(bc_ts)
+                transformations["Box-Cox Transformed"] = (
+                    boxcox_transformed_data,
+                    self.inv_boxcox,
+                )
 
             # Test variance stationarity for each transformed series
             best_transformation = None
@@ -171,9 +173,14 @@ class StationarityToolkit:
                 index=ts.index,
             )
             plt.figure(figsize=(10, 6))
-            self.df.plot(
-                title="Variance Stationarity with Best Transformation", figsize=(10, 6)
-            )
+            plt.plot(self.df.index, self.df.original, label="Original Plot")
+            plt.plot(self.df.index, self.df.var_transformed,
+                     label=f"{best_transformation_name} Variance Stationary Plot")
+            plt.title("Variance Stationarity Plot")
+            # self.df.plot(
+            #     title="Variance Stationarity with Best Transformation", figsize=(10, 6)
+            # )
+            plt.legend()
             plt.xlabel("Time")
             plt.ylabel("Values")
             plt.show()
@@ -188,7 +195,6 @@ class StationarityToolkit:
                 },
                 index=ts.index,
             )
-
         self._var_nonstationarity_removed = True
         return self.df
 
@@ -424,7 +430,7 @@ class StationarityToolkit:
     def _plot_trend_stationary_series(self):
         plt.figure(figsize=(10, 6))
         plt.plot(self.df.index, self.df.original, label="Original")
-        plt.plot(self.df.index, self.df.trend_transformed, label="Trend-Transformed")
+        plt.plot(self.df.index, self.df.trend_transformed, label=f"{self._differencing}-Transformed")
         plt.plot(title="Trend Stationarity", figsize=(10, 6))
         plt.legend()
         plt.xlabel("Time")

tests/stationarity_toolkit_test.py
@@ -29,8 +29,6 @@ def return_trend_variance_non_stationary_series():
 
     return df
 
-import numpy as np
-import pandas as pd
 
 def return_trend_seasonal_and_variance_nstationary_series():
     # Set a random seed for reproducibility
@@ -49,11 +47,11 @@ def return_trend_seasonal_and_variance_nstationary_series():
 
     # Generate noise with increasing variance
     noise_variance = np.linspace(0.1, 2.0, n)  # Increasing variance
-    noise = np.random.normal(0, noise_variance, n)
+    noise = np.abs(np.random.normal(0, noise_variance, n))  # Positive noise
 
     # Generate a seasonal pattern with increased amplitude
     seasonal_amplitude = 10  # Increased amplitude for more prominent seasonality
-    seasonal_pattern = seasonal_amplitude * np.sin(2 * np.pi * np.arange(n) / num_seasons)  # Seasonal pattern
+    seasonal_pattern = seasonal_amplitude * np.abs(np.sin(2 * np.pi * np.arange(n) / num_seasons + np.pi / 2))  # Shift phase to make it strictly positive
 
     # Combine the trend, seasonal pattern, and noise to create the time series
     time_series = trend + seasonal_pattern + noise
@@ -62,3 +60,8 @@ def return_trend_seasonal_and_variance_nstationary_series():
     df = pd.DataFrame({'Value': time_series}, index=time_index)
 
     return df
+
+def sample_real_dataset_test(csv_filepath: str):
+    real_dataset = pd.read_csv(csv_filepath, index_col=0)
+    real_dataset = real_dataset.fillna(0)
+    return real_dataset